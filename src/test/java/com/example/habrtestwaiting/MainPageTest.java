02cefb1d295431d3a18def4f593cb847c5c6049d
AlekseiVoropaev <aleksej.voropaev.93@mail.ru>
2023-08-21 16:58:16 +0300

    Changing the order of rows in the test "after"

--- src/test/java/com/example/habrtestwaiting/MainPageTest.java
+++ src/test/java/com/example/habrtestwaiting/MainPageTest.java
@@ -10,11 +10,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainPageTest {
     private WebDriver driver;
-
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
@@ -23,14 +21,11 @@ public class MainPageTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-
-
     }
     @AfterEach
     public void tearDown() {
         driver.quit();
     }
-
     @Test
     public void enable(){
         driver.get("https://demoqa.com/dynamic-properties");
@@ -41,13 +36,13 @@ public class MainPageTest {
     }
     @Test
     public void after() {
+        By visibleAfter = By.cssSelector("#visibleAfter");
         driver.get("https://demoqa.com/dynamic-properties");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(6));
-        WebElement visibleAfterButton = driver.findElement(By.cssSelector("#visibleAfter"));
-        wait.until(ExpectedConditions.visibilityOf(visibleAfterButton));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(visibleAfter));
+        WebElement visibleAfterButton = driver.findElement(visibleAfter);
         assertTrue(visibleAfterButton.isDisplayed()," Кнопка не видна ");
     }
-
 }
 
 
